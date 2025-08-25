fbbad522e0df0478714ab12d6a026d541717c8ed
laxatzi <laxatzi@yahoo.com>
2025-08-25 20:57:48 +0300

    loop through products

--- index.php
+++ index.php
@@ -11,7 +11,19 @@
 
 		$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		?>
-    <pre>
-<?php print_r($products); ?>
-		</pre>
+  <!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Products</title>
+	</head>
+	<body>
+		<h1>Products</h1>
+		<?php foreach ($products as $product): ?>
+			<h2><?= $product['name'] ?></h2>
+			<p><?= $product['description'] ?></p>
+    <?php endforeach; ?>
+	</body>
+	</html>
 
