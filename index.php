13d5299ed0541e3fa2f52d6fc1dc78790f0fe840
laxatzi <laxatzi@yahoo.com>
2025-08-25 17:23:11 +0300

    get data from database

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+		$dsn ="mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+
+		$pdo = new PDO($dsn, 'product_db_user', 'secret', [
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		]);
+
+		$stmt = $pdo->query("SELECT * FROM product");
+
+		$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		?>
+    <pre>
+<?php print_r($products); ?>
+		</pre>
+
