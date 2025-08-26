7cee075296f651cb2f011507b7842ff3f61cce4d
laxatzi <laxatzi@yahoo.com>
2025-08-26 12:25:17 +0300

    require model.php file

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 
-		$dsn ="mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
-
-		$pdo = new PDO($dsn, 'product_db_user', 'secret', [
-			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-		]);
-
-		$stmt = $pdo->query("SELECT * FROM product");
-
-		$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	require 'model.php';
+	$model = new Model();
+	$products = $model->getAllProducts();
 		?>
   <!DOCTYPE html>
 	<html lang="en">
