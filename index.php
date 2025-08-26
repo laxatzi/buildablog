0e82de8e38b42ac42b684d3abc01e626af941099
laxatzi <laxatzi@yahoo.com>
2025-08-26 12:00:24 +0300

    minor changes in file

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
 	<body>
 		<h1>Products</h1>
 		<?php foreach ($products as $product): ?>
-			<h2><?= $product['name'] ?></h2>
-			<p><?= $product['description'] ?></p>
+			<h2><?= htmlspecialchars($product['name']) ?></h2>
+			<p><?= htmlspecialchars($product['description']) ?></p>
     <?php endforeach; ?>
 	</body>
 	</html>
